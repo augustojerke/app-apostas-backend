a57da4338214293c2649bf6774ecf79f912d70f2
augustojerke <augustowieles@gmail.com>
2024-06-17 15:52:22 -0300

    init routes

---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+// router.post('/ranking', new RankingController().IncluirRanking)
+// router.get('/ranking', new RankingController().ListarRanking)
+
+export {router};
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express, {Request, Response, NextFunction} from 'express';
 const cors = require('cors');
+import { router } from './routes';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(3333, () => {
-    console.log("Server On");
+app.use(router)
+
+app.listen(3334, () => {
+    console.log("Server On - Porta: 3334");
 })
